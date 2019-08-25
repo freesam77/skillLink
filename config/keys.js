b9831db9491d4c7068efcfcb47096be82726a5f8
Samuel Razali <samuelrazali@gmail.com>
2019-08-25 23:16:32 +1000

    create config keys.js to connect mongoDB URI to server create routes

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:'mongodb://sam:14deepdive@ds257858.mlab.com:57858/skilllink'
+}
--- server.js
+++ server.js
@@ -1,8 +1,27 @@
-const express = require("express");
+const express = require("express"),
+  mongoose = require("mongoose"),
+  db = require("./config/keys").mongoURI;
+
+const users = require("./routes/api/users"),
+  profile = require("./routes/api/profile"),
+  posts = require("./routes/api/posts");
+
 const app = express();
 
-app.get('/', (req,res) => res.send("Hello World"));
+mongoose
+  .connect(db)
+  .then(() => console.log("mongoDB is connected"))
+  .catch(err => {
+    console.log(err);
+  });
 
-const port = process.env.PORT || 3000
+app.get("/", (req, res) => res.send("Hello World"));
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
